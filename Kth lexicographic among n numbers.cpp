c67203286cc4b392a027176bd42d8e7c3aff5687
Manvendra Pandey <45732132+vain-man@users.noreply.github.com>
2024-12-06 01:43:36 +0530

    Update Kth lexicographic among n numbers.cpp

--- Kth lexicographic among n numbers.cpp
+++ Kth lexicographic among n numbers.cpp
@@ -121,6 +121,40 @@ bool next_per(vii &arr, int n)
 	}
 	return false;
 }
+
+// Next permutation.
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n=nums.size();
+        int p=-1;
+        for(int i=n-2;i>=0;i--)
+        {
+            if(nums[i]<nums[i+1])
+            {
+                p=i;
+                break;
+            }
+        }
+        if(p==-1)
+        {
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+
+        for(int i=n-1;i>p;i--)
+        {
+            if(nums[i]>nums[p])
+            {
+                swap(nums[i], nums[p]);
+                break;
+            }
+        }
+        reverse(nums.begin()+p+1, nums.end());
+    }
+};
+
+
 void solve()
 {
 	int n, k;
